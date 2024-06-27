4aee37a5e75e030d44e166189d60c7d8bbb3a6d5
Arun <arunkumard0606@gmail.com>
2024-06-27 13:29:21 +0530

    Initial commit

---
+++ src/main/java/com/example/CRUD_MockitoTest/CrudMockitoTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.CRUD_MockitoTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMockitoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMockitoTestApplication.class, args);
+		System.out.println("App Started..");
+	}
+
+}
---
+++ src/test/java/com/example/CRUD_MockitoTest/CrudMockitoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_MockitoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMockitoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
